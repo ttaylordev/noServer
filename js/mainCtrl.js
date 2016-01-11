4ab1cf1151682276807de17bd5cd4f89fb9c1665
Tyson Taylor <mellowt@gmail.com>
2016-01-10 21:20:05 -0700

    it's painting, but coudl be optimized

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,5 @@
 angular.module('sappyAppy')
-  .controller('mainCtrl', function($scope, $firebaseObject, mainSvc) {  // inject dependancies, $scope, and service objects
+  .controller('mainCtrl', function($scope, $firebaseObject, mainSvc) { // inject dependancies, $scope, and service objects
     // adding/logging tests for mainCtrl to the DOM
     $scope.testMainCtrl = 'mainCtrl is working';
     console.log('MainCtrl is logging');
@@ -13,30 +13,69 @@ angular.module('sappyAppy')
     // declare firebase url and dom context.
     // includes logging for testing purposes.
     var baseUrl = 'https://blazing-inferno-1647.firebaseio.com//';
-    console.log('1. MainCtrl. ' + baseUrl);
-
     var ref = new Firebase(baseUrl + '/game-board');
     var fbo = $firebaseObject(ref); // allows us to write to a firebase object
 
-    console.log('2. MainCtrl. ',  $scope.context);
+    // gets the mouse position, and sends it to Firebase
+    $scope.canvas.addEventListener('mousemove', function(evt) { //snake case because it's an html thing.
+      if (mouseDown) {
+        var colorFlag = '#f34456';
+        var mousePos = mouseLoc($scope.canvas, evt);
+        console.log('!!!mouseX: ' + mousePos.x + ', mouseY: ' + mousePos.y);
+        // drawWithCursor(mousePos.x, mousePos.y);
+        freeDraw(mousePos.x, mousePos.y);
+        fbo.cursorStatus = {
+          x: mousePos.x,
+          y: mousePos.y,
+          color: colorFlag
+        };
+        fbo.$save(); //method to send.
+      }
+    }, false);
 
-    fbo.cursorLoc = $scope.context.getImageData(0, 0, 50, 50);
-    fbo.value = "This is a test";
-    fbo.$save(); //method to send.
+    //retrieves data from fire base, x, y values as well as color choice
+    ref.on("value", function(data) { //event handler for firebase, everytime they get new info they send it out to listeners
+      var exx = data.val().cursorStatus.x;
+      var why = data.val().cursorStatus.y;
+      var color = data.val().cursorStatus.color;
+      console.log('retrieved from firebase: ' + ',x: ' + exx + ',y: ' + why + ',color: ' + color);
+    });
+    // TODO: add users, and a switch statement to change the functionality of drawing with mouse, vs drawing from fbo data retrieved.
+    //tracks state of mouse button
+    var mouseDown = false;
+    $scope.canvas.addEventListener('mousedown', function(evt) { //snake case because it's an html thing.
+      mouseDown = true;
+    }, false);
+    $scope.canvas.addEventListener('mouseup', function(evt) { //snake case because it's an html thing.
+      mouseDown = false;
+    }, false);
 
-    console.log('3. MainCtrl. ', fbo);
 
-//return mouse location
-  function mouseLoc(canvas, evt){
-    var rect = canvas.getBoundingClientRect();
+    // console.log('2. MainCtrl. ', $scope.context);
 
-    return {
-      x:Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
-      y:Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)
-    };
+    // fbo.thingy = $scope.context.getImageData(0,0,50,50);
+    // fbo.value = "This is a test";
+    // fbo.$save(); //method to send.
+    //
+    // console.log('3. MainCtrl. ', fbo);
 
-  }
+    //return mouse location
+    function mouseLoc(canvas, evt) {
+      var rect = canvas.getBoundingClientRect();
+      return {
+        x: Math.round((evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width),
+        y: Math.round((evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
+      };
+    }
 
+    //freedrawing
+    function freeDraw(x, y) {
+      $scope.context.fillStyle = randomColor();
+      $scope.context.fillRect(x, y, 5, 5);
+      console.log(x);
+      console.log(y);
+      console.log('freeDraw is logging');
+    } //return typis is void, because there's no return statement
 
     // randomColor generator
     function randomColor() {
@@ -46,26 +85,30 @@ angular.module('sappyAppy')
       return 'rgb(' + rInt + ',' + gInt + ',' + bInt + ')';
     }
 
-      // draws random squares
+    // draws random squares
     function drawSquares() {
       $scope.context.fillStyle = randomColor();
       $scope.context.fillRect(Math.random() * 500, Math.random() * 500, Math.random() * 50, Math.random() * 50);
     }
 
     //draws by dragging cursor
+    //isnt working
     function drawWithCursor() {
       function strokeThat() {
-        $scope.context.strokeStyle = "white";
+        $scope.context.strokeStyle = '#f34456';
         $scope.context.lineWidth = 10;
+        console.log('strokin it');
       }
-      //draws lines
-      function drawLine() {
-        $scope.context.beginPath = "white";
-        $scope.context.moveTo = (100, 100);
-        $scope.context.lineTo(200, 200);
-        $scope.context.stroke();
-      }
+      strokeThat();
     }
+    //draws lines
+    function drawLine() {
+      $scope.context.beginPath = "white";
+      $scope.context.moveTo = (100, 100);
+      $scope.context.lineTo(200, 200);
+      $scope.context.stroke();
+    }
+
     // store a var with an onclick listener, an array, add this to the firebase array.
     // on the user side, listen for changes from firebase
     // console.log('5. ' + 'where is number 4?');
@@ -77,13 +120,13 @@ angular.module('sappyAppy')
 
   });
 
-  /* the controller is where data is stored to variables
-      to be accessed on the DOM.
-      #scope. is the required prefix for DOM passing
-      */
-  /* the controller also is responsible for running the
-      funtions stored elswhere in the object/filestructure
-      by assigning it to the $scope. It is a lazy manager,
-      who keeps track of work being performed, issues
-      commands and readies responses to be delivered to
-      the DOM*/
+/* the controller is where data is stored to variables
+    to be accessed on the DOM.
+    #scope. is the required prefix for DOM passing
+    */
+/* the controller also is responsible for running the
+    funtions stored elswhere in the object/filestructure
+    by assigning it to the $scope. It is a lazy manager,
+    who keeps track of work being performed, issues
+    commands and readies responses to be delivered to
+    the DOM*/
